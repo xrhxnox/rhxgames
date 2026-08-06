fc57c65449ebaa0000fc42881fd052d6fe630f42
Rix <nyjgtv47cn@privaterelay.appleid.com>
2026-08-06 02:50:47 -0700

    Editar: Splatoon 3

--- js/data.js
+++ js/data.js
@@ -340,8 +340,8 @@ const entries = [
     id: 53,
     titulo: "Splatoon 3",
     imagen: "assets/img/splatoon-3-53.jpg",
-    plataformas: ["switch2","pc"],
-    generos: ["Acción","Aventura","Shooter","Indie"],
+    plataformas: ["switch2"],
+    generos: ["Acción","Aventura","Shooter"],
     puntuacion: 8.5,
     fecha: "2026-08-06"
   },
