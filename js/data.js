9f47f3cef9208799cca81d7b0953813bd04a7dd2
Rix <nyjgtv47cn@privaterelay.appleid.com>
2026-08-06 17:52:15 -0700

    Editar: Animal Crossing: New Horizons

--- js/data.js
+++ js/data.js
@@ -44,8 +44,9 @@ const entries = [
     id: 4,
     titulo: "Animal Crossing: New Horizons",
     imagen: "assets/img/animal-crossing-new-horizons-4.png",
-    plataformas: ["switch","switch2"],
-    generos: ["Simulación"],
+    plataformas: ["gamecube","wii","switch","switch2","nds","pc"],
+    generos: ["Acción","Aventura","RPG","Estrategia","Plataformas","Simulación"],
+    top: "top10",
     puntuacion: 5,
     fecha: "2026-07-15"
   },
