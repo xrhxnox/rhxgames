90e3a84df9b266624adee22f7acb7cc1b9b49461
Rix <nyjgtv47cn@privaterelay.appleid.com>
2026-08-06 17:51:07 -0700

    Editar: No More Heroes

--- js/data.js
+++ js/data.js
@@ -215,8 +215,9 @@ const entries = [
     id: 39,
     titulo: "No More Heroes",
     imagen: "assets/img/no-more-heroes-39.jpg",
-    plataformas: ["wii","switch"],
-    generos: ["Acción"],
+    plataformas: ["gamecube","wii","switch","nds"],
+    generos: ["Acción","Aventura","RPG","Estrategia"],
+    top: "top10",
     puntuacion: 3.5,
     fecha: "2026-08-06"
   },
